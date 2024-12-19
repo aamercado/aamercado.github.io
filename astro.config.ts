f74a20753500a9591aea11bc62192e9048b914e1
aamercado <aamercado@hotmail.com>
2024-12-19 12:59:35 -0300

    agregando config

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
-
+  site: 'https://aamercado.github.io',
   integrations: [
     tailwind({
       applyBaseStyles: false,
